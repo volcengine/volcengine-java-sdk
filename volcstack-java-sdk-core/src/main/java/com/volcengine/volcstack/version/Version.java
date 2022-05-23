6f88fe5da4bde169c9bd9bd0589633addc4ebf2b
xuyaming <xuyaming@bytedance.com>
2022-05-23 12:11:01 +0800

    feat: for ecs 3.8.0

--- volcstack-java-sdk-core/src/main/java/com/volcengine/volcstack/version/Version.java
+++ volcstack-java-sdk-core/src/main/java/com/volcengine/volcstack/version/Version.java
@@ -1,6 +1,6 @@
 package com.volcengine.volcstack.version;
 
 public class Version {
-    public final static String SDK_VERSION = "0.1.0-SHAPSHOT";
+    public final static String SDK_VERSION = "0.1.0";
     public final static String SDK_NAME = "volcstack-java-sdk";
 }
