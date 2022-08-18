4157b924c2d470b46308b8b9b51a3f6d3f2d917a
xuyaming <xuyaming@bytedance.com>
2022-08-18 12:02:08 +0800

    feat: auto generate for for_iaas_3.9.1

--- volcengine-java-sdk-ecs/src/main/java/com/volcengine/ecs/model/DeleteInstanceRequest.java
+++ volcengine-java-sdk-ecs/src/main/java/com/volcengine/ecs/model/DeleteInstanceRequest.java
@@ -27,7 +27,7 @@ import javax.validation.Valid;
  * DeleteInstanceRequest
  */
 
-@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.JavaClientCodegen", date = "2022-08-15T17:09:47.140014+08:00[Asia/Shanghai]")
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.JavaClientCodegen", date = "2022-08-18T12:02:03.744330+08:00[Asia/Shanghai]")
 public class DeleteInstanceRequest {
   @SerializedName("InstanceId")
   private String instanceId = null;
