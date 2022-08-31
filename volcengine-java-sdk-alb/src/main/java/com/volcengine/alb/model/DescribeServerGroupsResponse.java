58c938508f9673a950a92902a61ab3cb54b1664d
邵士涛 <shaoshitao@bytedance.com>
2022-08-31 15:50:12 +0800

    feat: auto generate for shaoshitao_test

--- volcengine-java-sdk-alb/src/main/java/com/volcengine/alb/model/DescribeServerGroupsResponse.java
+++ volcengine-java-sdk-alb/src/main/java/com/volcengine/alb/model/DescribeServerGroupsResponse.java
@@ -30,7 +30,7 @@ import javax.validation.Valid;
  * DescribeServerGroupsResponse
  */
 
-@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.JavaClientCodegen", date = "2022-08-31T15:08:51.023818+08:00[Asia/Shanghai]")
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.JavaClientCodegen", date = "2022-08-31T15:50:05.609740+08:00[Asia/Shanghai]")
 public class DescribeServerGroupsResponse {
   @SerializedName("PageNumber")
   private Integer pageNumber = null;
--- volcengine-java-sdk-alb/src/main/java/com/volcengine/alb/model/ModifyRulesRequest.java
+++ volcengine-java-sdk-alb/src/main/java/com/volcengine/alb/model/ModifyRulesRequest.java
@@ -30,7 +30,7 @@ import javax.validation.Valid;
  * ModifyRulesRequest
  */
 
-@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.JavaClientCodegen", date = "2022-08-31T15:08:51.023818+08:00[Asia/Shanghai]")
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.JavaClientCodegen", date = "2022-08-31T15:50:05.609740+08:00[Asia/Shanghai]")
 public class ModifyRulesRequest {
   @SerializedName("ListenerId")
   private String listenerId = null;
