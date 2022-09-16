303f80e43e73ed901082b67b1bed043da70b44e5
薛鹏飞 <xuepengfei.xuepf@bytedance.com>
2022-09-16 11:30:46 +0800

    feat: auto generate for vke-2.4

--- volcengine-java-sdk-vke/src/main/java/com/volcengine/vke/model/TaintForCreateNodePoolInput.java
+++ volcengine-java-sdk-vke/src/main/java/com/volcengine/vke/model/TaintForCreateNodePoolInput.java
@@ -27,7 +27,7 @@ import javax.validation.Valid;
  * TaintForCreateNodePoolInput
  */
 
-@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.JavaClientCodegen", date = "2022-09-15T17:50:31.831766+08:00[Asia/Shanghai]")
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.JavaClientCodegen", date = "2022-09-16T11:30:28.141424+08:00[Asia/Shanghai]")
 public class TaintForCreateNodePoolInput {
   /**
    * Gets or Sets effect
