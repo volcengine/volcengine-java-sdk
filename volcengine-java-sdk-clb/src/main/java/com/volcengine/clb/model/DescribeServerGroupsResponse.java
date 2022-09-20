459951cc5b3367a3e3954e70f76e4524b98d03f8
xuyaming <xuyaming@bytedance.com>
2022-09-20 11:35:55 +0800

    feat: auto generate for for_iaas_3.10.0

--- volcengine-java-sdk-clb/src/main/java/com/volcengine/clb/model/DescribeServerGroupsResponse.java
+++ volcengine-java-sdk-clb/src/main/java/com/volcengine/clb/model/DescribeServerGroupsResponse.java
@@ -30,7 +30,7 @@ import javax.validation.Valid;
  * DescribeServerGroupsResponse
  */
 
-@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.JavaClientCodegen", date = "2022-07-15T17:10:28.835190+08:00[Asia/Shanghai]")
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.JavaClientCodegen", date = "2022-09-20T11:35:20.753228+08:00[Asia/Shanghai]")
 public class DescribeServerGroupsResponse {
   @SerializedName("PageNumber")
   private Integer pageNumber = null;
