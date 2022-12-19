58e6c3957efa20767029331fcbdc714287c76199
xuyaming <xuyaming@bytedance.com>
2022-12-20 00:04:37 +0800

    feat: update scm

--- volcengine-java-sdk-core/src/main/java/com/volcengine/model/VolcstackResponse.java
+++ volcengine-java-sdk-core/src/main/java/com/volcengine/model/VolcstackResponse.java
@@ -3,8 +3,8 @@ package com.volcengine.model;
 import com.google.gson.annotations.SerializedName;
 
 /**
- * @Author: xuyaming@bytedance.com
- * @Date: 2022/3/18 15:21
+ * xuyaming@bytedance.com
+ * 2022/3/18 15:21
  */
 public class VolcstackResponse<T> {
     @SerializedName("ResponseMetadata")
