4cc773c010bf56d0b75d00d57c5520776845a51f
ci_volc_sdk_gen <ci_volc_sdk_gen@bytedance.com>
2023-11-16 19:35:58 +0800

    feat: auto generate for alb-Java-2020-04-01-online-204-2023_11_09_15_45_43

--- volcengine-java-sdk-core/src/main/java/com/volcengine/version/Version.java
+++ volcengine-java-sdk-core/src/main/java/com/volcengine/version/Version.java
@@ -1,6 +1,6 @@
 package com.volcengine.version;
 
 public class Version {
-    public final static String SDK_VERSION = "0.1.89";
+    public final static String SDK_VERSION = "0.1.90";
     public final static String SDK_NAME = "volcengine-java-sdk";
 }
