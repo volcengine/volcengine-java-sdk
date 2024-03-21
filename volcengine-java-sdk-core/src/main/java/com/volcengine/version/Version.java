4a0932ce89bb0a2535416b97a6449c2a5d992537
ci_volc_sdk_gen <ci_volc_sdk_gen@bytedance.com>
2024-03-21 14:16:00 +0800

    feat: auto generate for vei_api-Java-2022-01-01-online-359-2024_03_21_14_15_40

--- volcengine-java-sdk-core/src/main/java/com/volcengine/version/Version.java
+++ volcengine-java-sdk-core/src/main/java/com/volcengine/version/Version.java
@@ -1,6 +1,6 @@
 package com.volcengine.version;
 
 public class Version {
-    public final static String SDK_VERSION = "0.1.101";
+    public final static String SDK_VERSION = "0.1.102";
     public final static String SDK_NAME = "volcengine-java-sdk";
 }
