1054da0ef877b8d861dc59180057004dd3db6163
liuweiwei.xy <liuweiwei.xy@bytedance.com>
2024-04-01 20:59:28 +0800

    feat: update

--- volcengine-java-sdk-core/src/main/java/com/volcengine/version/Version.java
+++ volcengine-java-sdk-core/src/main/java/com/volcengine/version/Version.java
@@ -1,6 +1,6 @@
 package com.volcengine.version;
 
 public class Version {
-    public final static String SDK_VERSION = "0.1.103";
+    public final static String SDK_VERSION = "0.1.104";
     public final static String SDK_NAME = "volcengine-java-sdk";
 }
