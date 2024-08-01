3d79682bcf59aa107ef901b55e78acf29d841c3e
ci_volc_sdk_gen <ci_volc_sdk_gen@bytedance.com>
2024-08-01 15:15:46 +0800

    feat: auto generate for kms-Java-2021-02-18-online-605-2024_08_01_15_15_11

--- volcengine-java-sdk-core/src/main/java/com/volcengine/version/Version.java
+++ volcengine-java-sdk-core/src/main/java/com/volcengine/version/Version.java
@@ -1,6 +1,6 @@
 package com.volcengine.version;
 
 public class Version {
-    public final static String SDK_VERSION = "0.1.122";
+    public final static String SDK_VERSION = "0.1.123";
     public final static String SDK_NAME = "volcengine-java-sdk";
 }
