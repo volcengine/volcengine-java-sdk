62600ccf3b85ac59b5e372ee78cbd7d4e26700d7
ci_volc_sdk_gen <ci_volc_sdk_gen@bytedance.com>
2024-11-13 17:46:00 +0800

    feat: auto generate for fw_center-Java-2021-09-06-online-769-2024_11_13_17_45_26

--- volcengine-java-sdk-core/src/main/java/com/volcengine/version/Version.java
+++ volcengine-java-sdk-core/src/main/java/com/volcengine/version/Version.java
@@ -1,6 +1,6 @@
 package com.volcengine.version;
 
 public class Version {
-    public final static String SDK_VERSION = "0.1.135";
+    public final static String SDK_VERSION = "0.1.136";
     public final static String SDK_NAME = "volcengine-java-sdk";
 }
