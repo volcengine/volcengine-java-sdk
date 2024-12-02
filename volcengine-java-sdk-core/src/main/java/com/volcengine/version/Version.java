b1feb457340a1392f42a593c8e00ff4a747c7e34
ci_volc_sdk_gen <ci_volc_sdk_gen@bytedance.com>
2024-12-02 15:40:38 +0800

    feat: auto generate for vefaas-Java-2024-06-06-online-814-2024_12_02_15_39_56

--- volcengine-java-sdk-core/src/main/java/com/volcengine/version/Version.java
+++ volcengine-java-sdk-core/src/main/java/com/volcengine/version/Version.java
@@ -1,6 +1,6 @@
 package com.volcengine.version;
 
 public class Version {
-    public final static String SDK_VERSION = "0.1.138";
+    public final static String SDK_VERSION = "0.1.139";
     public final static String SDK_NAME = "volcengine-java-sdk";
 }
