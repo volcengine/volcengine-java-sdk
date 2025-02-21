3debe15bb7124fbf6eb5b7352c3d2f71a34755e8
ci_volc_sdk_gen <ci_volc_sdk_gen@bytedance.com>
2025-02-21 16:09:07 +0800

    feat: auto generate for ark-Java-2024-01-01-online-987-2025_02_21_16_08_12

--- volcengine-java-sdk-core/src/main/java/com/volcengine/version/Version.java
+++ volcengine-java-sdk-core/src/main/java/com/volcengine/version/Version.java
@@ -1,6 +1,6 @@
 package com.volcengine.version;
 
 public class Version {
-    public final static String SDK_VERSION = "0.1.152";
+    public final static String SDK_VERSION = "0.1.153";
     public final static String SDK_NAME = "volcengine-java-sdk";
 }
