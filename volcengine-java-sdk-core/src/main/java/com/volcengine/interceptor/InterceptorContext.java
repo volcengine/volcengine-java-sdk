42f874b2756d4614674710c8a501a2ed4ca2015e
xiujie.1 <xiujie.1@bytedance.com>
2025-04-15 13:33:58 +0800

    feat: add interceptor chain

--- volcengine-java-sdk-core/src/main/java/com/volcengine/interceptor/InterceptorContext.java
+++ volcengine-java-sdk-core/src/main/java/com/volcengine/interceptor/InterceptorContext.java
@@ -91,7 +91,6 @@ public class InterceptorContext extends com.squareup.okhttp.Call {
         ServiceInfo serviceInfo = this.apiClient.addPairAndGetServiceInfo(path, queryParams, headerParams);
         //rebuild method
         method = serviceInfo.getMethod().toUpperCase();
-        String truePath = this.apiClient.getTruePath(path, headerParams);
         String contentType = headerParams.get("Content-Type");
         StringBuilder bodyBuilder = new StringBuilder();
         FormEncodingBuilder formBuilder = new FormEncodingBuilder();
@@ -140,7 +139,7 @@ public class InterceptorContext extends com.squareup.okhttp.Call {
         this.getRequestContext().setAuthNames(authNames);
         this.getRequestContext().setHeaderParams(headerParams);
         this.getRequestContext().setCommon(isCommon);
-        this.getRequestContext().setPath(truePath);
+        this.getRequestContext().setPath("/");
         this.getRequestContext().setMethod(method);
         this.getRequestContext().setServiceInfo(serviceInfo);
     }
