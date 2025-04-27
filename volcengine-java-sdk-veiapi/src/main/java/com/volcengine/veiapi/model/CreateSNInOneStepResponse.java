12d0352cca3ebba4238c9bc09105b57aa5100283
ci_volc_sdk_gen <ci_volc_sdk_gen@bytedance.com>
2025-04-27 14:18:39 +0800

    feat: auto generate for vei_api-Java-2022-01-01-online-1132-2025_04_27_14_18_00

--- volcengine-java-sdk-veiapi/src/main/java/com/volcengine/veiapi/model/CreateSNInOneStepResponse.java
+++ volcengine-java-sdk-veiapi/src/main/java/com/volcengine/veiapi/model/CreateSNInOneStepResponse.java
@@ -28,6 +28,7 @@ import javax.validation.Valid;
  */
 
 
+
 public class CreateSNInOneStepResponse extends com.volcengine.model.AbstractResponse {
   @SerializedName("node_id")
   private String nodeId = null;
