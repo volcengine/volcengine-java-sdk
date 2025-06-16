1abf590221db5b2cd0de01b519da4349df3bed78
ci_volc_sdk_gen <ci_volc_sdk_gen@bytedance.com>
2025-06-16 16:21:42 +0800

    feat: auto generate for coze-Java-2025-06-01-online-1289-2025_06_16_16_21_11

--- volcengine-java-sdk-core/src/main/java/com/volcengine/version/Version.java
+++ volcengine-java-sdk-core/src/main/java/com/volcengine/version/Version.java
@@ -1,6 +1,6 @@
 package com.volcengine.version;
 
 public class Version {
-    public final static String SDK_VERSION = "0.2.16";
+    public final static String SDK_VERSION = "0.2.17";
     public final static String SDK_NAME = "volcengine-java-sdk";
 }
