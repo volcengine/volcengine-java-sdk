ed2e3fcf0f1e8073816db4b76a755d4215f3bdd2
lishuxiang <lishuxiang@bytedance.com>
2025-07-07 01:57:21 +0800

    feat(proxy): support http(s) proxy

--- volcengine-java-sdk-core/src/main/java/com/volcengine/ApiClient.java
+++ volcengine-java-sdk-core/src/main/java/com/volcengine/ApiClient.java
@@ -72,8 +72,7 @@ import java.io.UnsupportedEncodingException;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.lang.reflect.Type;
-import java.net.URLConnection;
-import java.net.URLEncoder;
+import java.net.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.security.GeneralSecurityException;
@@ -141,8 +140,11 @@ public class ApiClient extends BaseClient{
     private Boolean useDualStack;
 
     private boolean autoRetry = DefaultRetryerSetting.DEFAULT_AUTO_RETRY_ENABLED;
-
     private final Retryer retryer = DefaultRetryerSetting.DEFAULT_RETRYER;
+
+    private String httpProxy;
+    private String httpsProxy;
+    private String noProxy;
 
     /*
      * Constructor for ApiClient
@@ -151,8 +153,6 @@ public class ApiClient extends BaseClient{
         ConnectionPool connectionPool = new ConnectionPool(maxIdleConns, keepAliveDurationMs);
         httpClient = new OkHttpClient();
         httpClient.setConnectionPool(connectionPool);
-
-
         verifyingSsl = true;
 
         json = new JSON();
@@ -591,23 +591,128 @@ public class ApiClient extends BaseClient{
     }
 
     /**
-     * Get the use dual stack flag.
+     * Get the http proxy.
      *
-     * @return use dual stack flag
+     * @return http proxy
      */
-    public Boolean getUseDualStack() {
-        return this.useDualStack;
+    public String getHttpProxy() {
+        return this.httpProxy;
     }
 
     /**
-     * Set the use dual stack flag.
+     * Set the http proxy.
      *
-     * @param useDualStack boolean
      * @return Api client
      */
-    public ApiClient setUseDualStack(boolean useDualStack) {
-        this.useDualStack = useDualStack;
+    public ApiClient setHttpProxy(String httpProxy) {
+        this.httpProxy = httpProxy;
+        updateClientProxy();
         return this;
+    }
+
+    /**
+     * Get the https proxy.
+     *
+     * @return https proxy
+     */
+    public String getHttpsProxy() {
+        return this.httpsProxy;
+    }
+
+    /**
+     * Set the https proxy.
+     *
+     * @return Api client
+     */
+    public ApiClient setHttpsProxy(String httpsProxy) {
+        this.httpsProxy = httpsProxy;
+        updateClientProxy();
+        return this;
+    }
+
+    /**
+     * Get the no proxy.
+     *
+     * @return no proxy
+     */
+    public String getNoProxy() {
+        return this.noProxy;
+    }
+
+    /**
+     * Set the no proxy.
+     *
+     * @return Api client
+     */
+    public ApiClient setNoProxy(String noProxy) {
+        this.noProxy = noProxy;
+        updateClientProxy();
+        return this;
+    }
+
+    private void updateClientProxy() {
+        httpClient.setProxySelector(new ProxySelector() {
+            @Override
+            public List<Proxy> select(URI uri) {
+                String targetHost = uri.getHost();
+
+                String envNoProxy = System.getenv("NO_PROXY");
+                if (StringUtils.isEmpty(envNoProxy)) {
+                    envNoProxy = System.getenv("no_proxy");
+                }
+
+                boolean noProxyFlag = false;
+                String noProxyList = (StringUtils.isEmpty(noProxy) ? envNoProxy : noProxy);
+                if (!StringUtils.isEmpty(noProxyList)) {
+                    String[] noProxyArr = noProxyList.split(",");
+                    for (String noProxyHost : noProxyArr) {
+                        if (noProxyHost.equals(targetHost)) {
+                            noProxyFlag = true;
+                            break;
+                        }
+                    }
+                }
+
+                List<Proxy> proxies = new ArrayList<>();
+                if (noProxyFlag) {
+                    proxies.add(Proxy.NO_PROXY);
+                    return proxies;
+                }
+
+                addProxy(proxies, httpProxy, "HTTP_PROXY");
+                addProxy(proxies, httpsProxy, "HTTPS_PROXY");
+
+                return proxies;
+            }
+
+            @Override
+            public void connectFailed(URI uri, SocketAddress sa, IOException ioe) {
+            }
+        });
+    }
+
+    private void addProxy(List<Proxy> proxies, String proxy, String env) {
+        if (!StringUtils.isEmpty(proxy)) {
+            try {
+                URI u = new URI(proxy);
+                proxies.add(new Proxy(Proxy.Type.HTTP, new InetSocketAddress(u.getHost(), u.getPort())));
+            } catch (URISyntaxException e) {
+                throw new RuntimeException(e);
+            }
+        } else if (!StringUtils.isEmpty(env)) {
+            String envProxy = System.getenv(env.toUpperCase());
+            if (StringUtils.isEmpty(envProxy)) {
+                envProxy = System.getenv(env.toLowerCase());
+            }
+            if (!StringUtils.isEmpty(envProxy)) {
+                try {
+                    URI u = new URI(envProxy);
+                    proxies.add(new Proxy(Proxy.Type.HTTP, new InetSocketAddress(u.getHost(), u.getPort())));
+                } catch (URISyntaxException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
     }
 
     /**
