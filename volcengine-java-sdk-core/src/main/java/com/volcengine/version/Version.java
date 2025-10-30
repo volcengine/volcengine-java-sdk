9311f2349ed1b35c1b6716d1f5501add7a237368
xiujie.1 <xiujie.1@bytedance.com>
2025-10-30 18:52:45 +0800

    update version and commitId

--- volcengine-java-sdk-core/src/main/java/com/volcengine/version/Version.java
+++ volcengine-java-sdk-core/src/main/java/com/volcengine/version/Version.java
@@ -1,6 +1,6 @@
 package com.volcengine.version;
 
 public class Version {
-    public final static String SDK_VERSION = "0.2.41";
+    public final static String SDK_VERSION = "0.2.43";
     public final static String SDK_NAME = "volcengine-java-sdk";
 }
