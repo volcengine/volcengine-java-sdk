d713c56eb9aabeb8097521e30bea53faa48b6251
Jialun Peng <p1070048431@gmail.com>
2025-11-03 23:13:57 +0800

    feat: add Object type overloads for content and toolChoice in Builder
    - Add content(Object) method to ChatMessage.Builder
    - Add toolChoice(Object) method to ChatCompletionRequest.Builder

--- volcengine-java-sdk-ark-runtime/src/main/java/com/volcengine/ark/runtime/model/completion/chat/ChatMessage.java
+++ volcengine-java-sdk-ark-runtime/src/main/java/com/volcengine/ark/runtime/model/completion/chat/ChatMessage.java
@@ -138,6 +138,11 @@ public class ChatMessage {
 			return this;
 		}
 
+        public ChatMessage.Builder content(Object content) {
+            this.content = content;
+            return this;
+        }
+
 		public ChatMessage.Builder multiContent(List<ChatCompletionContentPart> multiContent) {
 			this.content = multiContent;
 			return this;
