7557be5edf781540220020d7fd74abe153178f3e
ci_volc_sdk_gen <ci_volc_sdk_gen@bytedance.com>
2025-11-11 15:53:58 +0800

    feat: auto generate for graph-Java-2018-01-01-online-1799-2025_11_11_15_53_23

--- volcengine-java-sdk-graph/src/main/java/com/volcengine/graph/model/ColumnForGetTableSchemaOutput.java
+++ volcengine-java-sdk-graph/src/main/java/com/volcengine/graph/model/ColumnForGetTableSchemaOutput.java
@@ -42,6 +42,9 @@ public class ColumnForGetTableSchemaOutput {
   @SerializedName("schema_column_type")
   private String schemaColumnType = null;
 
+  @SerializedName("vector_size")
+  private Integer vectorSize = null;
+
   public ColumnForGetTableSchemaOutput columnName(String columnName) {
     this.columnName = columnName;
     return this;
@@ -114,6 +117,24 @@ public class ColumnForGetTableSchemaOutput {
     this.schemaColumnType = schemaColumnType;
   }
 
+  public ColumnForGetTableSchemaOutput vectorSize(Integer vectorSize) {
+    this.vectorSize = vectorSize;
+    return this;
+  }
+
+   /**
+   * Get vectorSize
+   * @return vectorSize
+  **/
+  @Schema(description = "")
+  public Integer getVectorSize() {
+    return vectorSize;
+  }
+
+  public void setVectorSize(Integer vectorSize) {
+    this.vectorSize = vectorSize;
+  }
+
 
   @Override
   public boolean equals(java.lang.Object o) {
@@ -127,12 +148,13 @@ public class ColumnForGetTableSchemaOutput {
     return Objects.equals(this.columnName, columnForGetTableSchemaOutput.columnName) &&
         Objects.equals(this.defaultValue, columnForGetTableSchemaOutput.defaultValue) &&
         Objects.equals(this.enableDefaultValue, columnForGetTableSchemaOutput.enableDefaultValue) &&
-        Objects.equals(this.schemaColumnType, columnForGetTableSchemaOutput.schemaColumnType);
+        Objects.equals(this.schemaColumnType, columnForGetTableSchemaOutput.schemaColumnType) &&
+        Objects.equals(this.vectorSize, columnForGetTableSchemaOutput.vectorSize);
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(columnName, defaultValue, enableDefaultValue, schemaColumnType);
+    return Objects.hash(columnName, defaultValue, enableDefaultValue, schemaColumnType, vectorSize);
   }
 
 
@@ -145,6 +167,7 @@ public class ColumnForGetTableSchemaOutput {
     sb.append("    defaultValue: ").append(toIndentedString(defaultValue)).append("\n");
     sb.append("    enableDefaultValue: ").append(toIndentedString(enableDefaultValue)).append("\n");
     sb.append("    schemaColumnType: ").append(toIndentedString(schemaColumnType)).append("\n");
+    sb.append("    vectorSize: ").append(toIndentedString(vectorSize)).append("\n");
     sb.append("}");
     return sb.toString();
   }
