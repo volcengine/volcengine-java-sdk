b9856ed056e5ff96ebc396c8737978a2607c4b21
ci_volc_sdk_gen <ci_volc_sdk_gen@bytedance.com>
2025-11-20 15:11:42 +0800

    feat: auto generate for pca-Java-2025-10-01-online-1859-2025_11_20_15_10_59

--- volcengine-java-sdk-core/src/main/java/com/volcengine/version/Version.java
+++ volcengine-java-sdk-core/src/main/java/com/volcengine/version/Version.java
@@ -1,6 +1,6 @@
 package com.volcengine.version;
 
 public class Version {
-    public final static String SDK_VERSION = "0.2.46";
+    public final static String SDK_VERSION = "0.2.47";
     public final static String SDK_NAME = "volcengine-java-sdk";
 }
