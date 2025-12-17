d53e8517d097e038b9405a570908c4fde930a9db
ci_volc_sdk_gen <ci_volc_sdk_gen@bytedance.com>
2025-12-17 21:15:59 +0800

    feat: auto generate for dts-Java-2018-01-01-online-1956-2025_12_17_21_15_19

--- volcengine-java-sdk-core/src/main/java/com/volcengine/version/Version.java
+++ volcengine-java-sdk-core/src/main/java/com/volcengine/version/Version.java
@@ -1,6 +1,6 @@
 package com.volcengine.version;
 
 public class Version {
-    public final static String SDK_VERSION = "0.2.53";
+    public final static String SDK_VERSION = "0.2.54";
     public final static String SDK_NAME = "volcengine-java-sdk";
 }
