0df071593cb8d2ce059c3c06d48c9e061c5c5b66
ci_volc_sdk_gen <ci_volc_sdk_gen@bytedance.com>
2025-12-29 15:12:25 +0800

    feat: auto generate for flink-Java-2025-01-01-online-1985-2025_12_29_15_11_50

--- volcengine-java-sdk-core/src/main/java/com/volcengine/version/Version.java
+++ volcengine-java-sdk-core/src/main/java/com/volcengine/version/Version.java
@@ -1,6 +1,6 @@
 package com.volcengine.version;
 
 public class Version {
-    public final static String SDK_VERSION = "1.0.1";
+    public final static String SDK_VERSION = "1.0.2";
     public final static String SDK_NAME = "volcengine-java-sdk";
 }
