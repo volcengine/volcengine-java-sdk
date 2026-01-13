4664eb771e95cef66cbde13a99770933fe5f2c28
ci_volc_sdk_gen <ci_volc_sdk_gen@bytedance.com>
2026-01-13 17:53:41 +0800

    feat: auto generate for cloud_detect-Java-2025-10-31-online-2031-2026_01_13_17_52_55

--- volcengine-java-sdk-core/src/main/java/com/volcengine/version/Version.java
+++ volcengine-java-sdk-core/src/main/java/com/volcengine/version/Version.java
@@ -1,6 +1,6 @@
 package com.volcengine.version;
 
 public class Version {
-    public final static String SDK_VERSION = "1.0.5";
+    public final static String SDK_VERSION = "1.0.6";
     public final static String SDK_NAME = "volcengine-java-sdk";
 }
