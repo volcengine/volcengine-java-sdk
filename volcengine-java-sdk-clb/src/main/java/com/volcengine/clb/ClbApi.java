d3a3e55270cb28c001d6d181d4370ab83b8a2920
ci_volc_sdk_gen <ci_volc_sdk_gen@bytedance.com>
2026-02-03 16:37:51 +0800

    feat: auto generate for clb-Java-2020-04-01-online-2247-2026_02_03_16_37_09

--- volcengine-java-sdk-clb/src/main/java/com/volcengine/clb/ClbApi.java
+++ volcengine-java-sdk-clb/src/main/java/com/volcengine/clb/ClbApi.java
@@ -99,6 +99,8 @@ import com.volcengine.clb.model.DescribeLoadBalancerAttributesRequest;
 import com.volcengine.clb.model.DescribeLoadBalancerAttributesResponse;
 import com.volcengine.clb.model.DescribeLoadBalancerSpecsRequest;
 import com.volcengine.clb.model.DescribeLoadBalancerSpecsResponse;
+import com.volcengine.clb.model.DescribeLoadBalancerStatusRequest;
+import com.volcengine.clb.model.DescribeLoadBalancerStatusResponse;
 import com.volcengine.clb.model.DescribeLoadBalancersBillingRequest;
 import com.volcengine.clb.model.DescribeLoadBalancersBillingResponse;
 import com.volcengine.clb.model.DescribeLoadBalancersRequest;
@@ -4686,6 +4688,130 @@ public class ClbApi {
 
         com.squareup.okhttp.Call call = describeLoadBalancerSpecsValidateBeforeCall(body, progressListener, progressRequestListener);
         Type localVarReturnType = new TypeToken<DescribeLoadBalancerSpecsResponse>(){}.getType();
+        apiClient.executeAsync(call, localVarReturnType, callback);
+        return call;
+    }
+    /**
+     * Build call for describeLoadBalancerStatus
+     * @param body  (required)
+     * @param progressListener Progress listener
+     * @param progressRequestListener Progress request listener
+     * @return Call to execute
+     * @throws ApiException If fail to serialize the request body object
+     */
+    public com.squareup.okhttp.Call describeLoadBalancerStatusCall(DescribeLoadBalancerStatusRequest body, final ProgressResponseBody.ProgressListener progressListener, final ProgressRequestBody.ProgressRequestListener progressRequestListener) throws ApiException {
+        Object localVarPostBody = body;
+        
+        // create path and map variables
+        String localVarPath = "/DescribeLoadBalancerStatus/2020-04-01/clb/get/text_plain/";
+
+        List<Pair> localVarQueryParams = new ArrayList<Pair>();
+        List<Pair> localVarCollectionQueryParams = new ArrayList<Pair>();
+
+        Map<String, String> localVarHeaderParams = new HashMap<String, String>();
+
+        Map<String, Object> localVarFormParams = new HashMap<String, Object>();
+
+        final String[] localVarAccepts = {
+            "application/json"
+        };
+        final String localVarAccept = apiClient.selectHeaderAccept(localVarAccepts);
+        if (localVarAccept != null) localVarHeaderParams.put("Accept", localVarAccept);
+
+        final String[] localVarContentTypes = {
+            "text/plain"
+        };
+        final String localVarContentType = apiClient.selectHeaderContentType(localVarContentTypes);
+        localVarHeaderParams.put("Content-Type", localVarContentType);
+
+        if(progressListener != null) {
+            apiClient.getHttpClient().networkInterceptors().add(new com.squareup.okhttp.Interceptor() {
+                @Override
+                public com.squareup.okhttp.Response intercept(com.squareup.okhttp.Interceptor.Chain chain) throws IOException {
+                    com.squareup.okhttp.Response originalResponse = chain.proceed(chain.request());
+                    return originalResponse.newBuilder()
+                    .body(new ProgressResponseBody(originalResponse.body(), progressListener))
+                    .build();
+                }
+            });
+        }
+
+        String[] localVarAuthNames = new String[] { "volcengineSign" };
+        return apiClient.buildCall(localVarPath, "POST", localVarQueryParams, localVarCollectionQueryParams, localVarPostBody, localVarHeaderParams, localVarFormParams, localVarAuthNames, progressRequestListener);
+    }
+    
+    @SuppressWarnings("rawtypes")
+    private com.squareup.okhttp.Call describeLoadBalancerStatusValidateBeforeCall(DescribeLoadBalancerStatusRequest body, final ProgressResponseBody.ProgressListener progressListener, final ProgressRequestBody.ProgressRequestListener progressRequestListener) throws ApiException {
+        // verify the required parameter 'body' is set
+        if (body == null) {
+            throw new ApiException("Missing the required parameter 'body' when calling describeLoadBalancerStatus(Async)");
+        }
+        
+        com.squareup.okhttp.Call call = describeLoadBalancerStatusCall(body, progressListener, progressRequestListener);
+        return call;
+
+        
+        
+        
+        
+    }
+
+    /**
+     * 
+     * 
+     * @param body  (required)
+     * @return DescribeLoadBalancerStatusResponse
+     * @throws ApiException If fail to call the API, e.g. server error or cannot deserialize the response body
+     */
+    public DescribeLoadBalancerStatusResponse describeLoadBalancerStatus(DescribeLoadBalancerStatusRequest body) throws ApiException {
+        ApiResponse<DescribeLoadBalancerStatusResponse> resp = describeLoadBalancerStatusWithHttpInfo(body);
+        return resp.getData();
+    }
+
+    /**
+     * 
+     * 
+     * @param body  (required)
+     * @return ApiResponse&lt;DescribeLoadBalancerStatusResponse&gt;
+     * @throws ApiException If fail to call the API, e.g. server error or cannot deserialize the response body
+     */
+    public ApiResponse<DescribeLoadBalancerStatusResponse> describeLoadBalancerStatusWithHttpInfo( @NotNull DescribeLoadBalancerStatusRequest body) throws ApiException {
+        com.squareup.okhttp.Call call = describeLoadBalancerStatusValidateBeforeCall(body, null, null);
+        Type localVarReturnType = new TypeToken<DescribeLoadBalancerStatusResponse>(){}.getType();
+        return apiClient.execute(call, localVarReturnType);
+    }
+
+    /**
+     *  (asynchronously)
+     * 
+     * @param body  (required)
+     * @param callback The callback to be executed when the API call finishes
+     * @return The request call
+     * @throws ApiException If fail to process the API call, e.g. serializing the request body object
+     */
+    public com.squareup.okhttp.Call describeLoadBalancerStatusAsync(DescribeLoadBalancerStatusRequest body, final ApiCallback<DescribeLoadBalancerStatusResponse> callback) throws ApiException {
+
+        ProgressResponseBody.ProgressListener progressListener = null;
+        ProgressRequestBody.ProgressRequestListener progressRequestListener = null;
+
+        if (callback != null) {
+            progressListener = new ProgressResponseBody.ProgressListener() {
+                @Override
+                public void update(long bytesRead, long contentLength, boolean done) {
+                    callback.onDownloadProgress(bytesRead, contentLength, done);
+                }
+            };
+
+            progressRequestListener = new ProgressRequestBody.ProgressRequestListener() {
+                @Override
+                public void onRequestProgress(long bytesWritten, long contentLength, boolean done) {
+                    callback.onUploadProgress(bytesWritten, contentLength, done);
+                }
+            };
+        }
+
+        com.squareup.okhttp.Call call = describeLoadBalancerStatusValidateBeforeCall(body, progressListener, progressRequestListener);
+        Type localVarReturnType = new TypeToken<DescribeLoadBalancerStatusResponse>(){}.getType();
         apiClient.executeAsync(call, localVarReturnType, callback);
         return call;
     }
