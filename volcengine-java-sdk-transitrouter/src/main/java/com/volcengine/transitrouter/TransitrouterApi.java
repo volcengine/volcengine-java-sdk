da88b00d80ca4a7e6debf4ae8554e4d19bdfbaa3
ci_volc_sdk_gen <ci_volc_sdk_gen@bytedance.com>
2026-07-07 20:51:15 +0800

    feat: auto generate for transitrouter-Java-2020-04-01-online-2886-2026_07_07_20_50_30

--- volcengine-java-sdk-transitrouter/src/main/java/com/volcengine/transitrouter/TransitrouterApi.java
+++ volcengine-java-sdk-transitrouter/src/main/java/com/volcengine/transitrouter/TransitrouterApi.java
@@ -127,6 +127,8 @@ import com.volcengine.transitrouter.model.DeleteTransitRouterVpcAttachmentReques
 import com.volcengine.transitrouter.model.DeleteTransitRouterVpcAttachmentResponse;
 import com.volcengine.transitrouter.model.DeleteTransitRouterVpnAttachmentRequest;
 import com.volcengine.transitrouter.model.DeleteTransitRouterVpnAttachmentResponse;
+import com.volcengine.transitrouter.model.Describe95TrafficRequest;
+import com.volcengine.transitrouter.model.Describe95TrafficResponse;
 import com.volcengine.transitrouter.model.DescribeTransitRouterAttachmentsRequest;
 import com.volcengine.transitrouter.model.DescribeTransitRouterAttachmentsResponse;
 import com.volcengine.transitrouter.model.DescribeTransitRouterBandwidthPackagesBillingRequest;
@@ -6482,6 +6484,130 @@ public class TransitrouterApi {
 
         com.squareup.okhttp.Call call = deleteTransitRouterVpnAttachmentValidateBeforeCall(body, progressListener, progressRequestListener);
         Type localVarReturnType = new TypeToken<DeleteTransitRouterVpnAttachmentResponse>(){}.getType();
+        apiClient.executeAsync(call, localVarReturnType, callback);
+        return call;
+    }
+    /**
+     * Build call for describe95Traffic
+     * @param body  (required)
+     * @param progressListener Progress listener
+     * @param progressRequestListener Progress request listener
+     * @return Call to execute
+     * @throws ApiException If fail to serialize the request body object
+     */
+    public com.squareup.okhttp.Call describe95TrafficCall(Describe95TrafficRequest body, final ProgressResponseBody.ProgressListener progressListener, final ProgressRequestBody.ProgressRequestListener progressRequestListener) throws ApiException {
+        Object localVarPostBody = body;
+        
+        // create path and map variables
+        String localVarPath = "/Describe95Traffic/2020-04-01/transitrouter/get/text_plain/";
+
+        List<Pair> localVarQueryParams = new ArrayList<Pair>();
+        List<Pair> localVarCollectionQueryParams = new ArrayList<Pair>();
+
+        Map<String, String> localVarHeaderParams = new HashMap<String, String>();
+
+        Map<String, Object> localVarFormParams = new HashMap<String, Object>();
+
+        final String[] localVarAccepts = {
+            "application/json"
+        };
+        final String localVarAccept = apiClient.selectHeaderAccept(localVarAccepts);
+        if (localVarAccept != null) localVarHeaderParams.put("Accept", localVarAccept);
+
+        final String[] localVarContentTypes = {
+            "text/plain"
+        };
+        final String localVarContentType = apiClient.selectHeaderContentType(localVarContentTypes);
+        localVarHeaderParams.put("Content-Type", localVarContentType);
+
+        if(progressListener != null) {
+            apiClient.getHttpClient().networkInterceptors().add(new com.squareup.okhttp.Interceptor() {
+                @Override
+                public com.squareup.okhttp.Response intercept(com.squareup.okhttp.Interceptor.Chain chain) throws IOException {
+                    com.squareup.okhttp.Response originalResponse = chain.proceed(chain.request());
+                    return originalResponse.newBuilder()
+                    .body(new ProgressResponseBody(originalResponse.body(), progressListener))
+                    .build();
+                }
+            });
+        }
+
+        String[] localVarAuthNames = new String[] { "volcengineSign" };
+        return apiClient.buildCall(localVarPath, "POST", localVarQueryParams, localVarCollectionQueryParams, localVarPostBody, localVarHeaderParams, localVarFormParams, localVarAuthNames, progressRequestListener);
+    }
+    
+    @SuppressWarnings("rawtypes")
+    private com.squareup.okhttp.Call describe95TrafficValidateBeforeCall(Describe95TrafficRequest body, final ProgressResponseBody.ProgressListener progressListener, final ProgressRequestBody.ProgressRequestListener progressRequestListener) throws ApiException {
+        // verify the required parameter 'body' is set
+        if (body == null) {
+            throw new ApiException("Missing the required parameter 'body' when calling describe95Traffic(Async)");
+        }
+        
+        com.squareup.okhttp.Call call = describe95TrafficCall(body, progressListener, progressRequestListener);
+        return call;
+
+        
+        
+        
+        
+    }
+
+    /**
+     * 
+     * 
+     * @param body  (required)
+     * @return Describe95TrafficResponse
+     * @throws ApiException If fail to call the API, e.g. server error or cannot deserialize the response body
+     */
+    public Describe95TrafficResponse describe95Traffic(Describe95TrafficRequest body) throws ApiException {
+        ApiResponse<Describe95TrafficResponse> resp = describe95TrafficWithHttpInfo(body);
+        return resp.getData();
+    }
+
+    /**
+     * 
+     * 
+     * @param body  (required)
+     * @return ApiResponse&lt;Describe95TrafficResponse&gt;
+     * @throws ApiException If fail to call the API, e.g. server error or cannot deserialize the response body
+     */
+    public ApiResponse<Describe95TrafficResponse> describe95TrafficWithHttpInfo( @NotNull Describe95TrafficRequest body) throws ApiException {
+        com.squareup.okhttp.Call call = describe95TrafficValidateBeforeCall(body, null, null);
+        Type localVarReturnType = new TypeToken<Describe95TrafficResponse>(){}.getType();
+        return apiClient.execute(call, localVarReturnType);
+    }
+
+    /**
+     *  (asynchronously)
+     * 
+     * @param body  (required)
+     * @param callback The callback to be executed when the API call finishes
+     * @return The request call
+     * @throws ApiException If fail to process the API call, e.g. serializing the request body object
+     */
+    public com.squareup.okhttp.Call describe95TrafficAsync(Describe95TrafficRequest body, final ApiCallback<Describe95TrafficResponse> callback) throws ApiException {
+
+        ProgressResponseBody.ProgressListener progressListener = null;
+        ProgressRequestBody.ProgressRequestListener progressRequestListener = null;
+
+        if (callback != null) {
+            progressListener = new ProgressResponseBody.ProgressListener() {
+                @Override
+                public void update(long bytesRead, long contentLength, boolean done) {
+                    callback.onDownloadProgress(bytesRead, contentLength, done);
+                }
+            };
+
+            progressRequestListener = new ProgressRequestBody.ProgressRequestListener() {
+                @Override
+                public void onRequestProgress(long bytesWritten, long contentLength, boolean done) {
+                    callback.onUploadProgress(bytesWritten, contentLength, done);
+                }
+            };
+        }
+
+        com.squareup.okhttp.Call call = describe95TrafficValidateBeforeCall(body, progressListener, progressRequestListener);
+        Type localVarReturnType = new TypeToken<Describe95TrafficResponse>(){}.getType();
         apiClient.executeAsync(call, localVarReturnType, callback);
         return call;
     }
