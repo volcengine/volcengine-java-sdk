c109e405089e5e7a696ec8053d5745378d4f71d2
ci_volc_sdk_gen <ci_volc_sdk_gen@bytedance.com>
2026-07-08 15:57:02 +0800

    feat: auto generate for na-Java-2020-04-01-online-2888-2026_07_08_15_56_33

--- volcengine-java-sdk-core/src/main/java/com/volcengine/endpoint/DefaultEndpointProvider.java
+++ volcengine-java-sdk-core/src/main/java/com/volcengine/endpoint/DefaultEndpointProvider.java
@@ -828,6 +828,14 @@ public class DefaultEndpointProvider implements EndpointResolver {
                 ENDPOINT,
                 createRegionEndpointMap()
         ));
+        // --------------------------- na ---------------------------
+        DEFAULT_ENDPOINT_MAP.put("na", new ServiceEndpointInfo(
+                "na",
+                true,
+                "",
+                ENDPOINT,
+                createRegionEndpointMap()
+        ));
     }
 
     // 创建区域端点映射的辅助方法
